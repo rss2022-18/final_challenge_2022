b8dd93a75309be765d8ed566ea7c014537ff1e20
reyfp <rey40fp@gmail.com>
2022-04-28 14:35:33 +0000

    Stop Sign detection

--- city_driving/stop_detector/stop_detector.py
+++ city_driving/stop_detector/stop_detector.py
@@ -3,12 +3,13 @@ import rospy
 
 import numpy as np
 from sensor_msgs.msg import Image
+from std_msgs.msg import Float32MultiArray
 from detector import StopSignDetector
 
 class SignDetector:
     def __init__(self):
         self.detector = StopSignDetector()
-        self.publisher = None #TODO
+        self.publisher = rospy.Publisher("/stop_detection", Float32MultiArray, queue_size=10)
         self.subscriber = rospy.Subscriber("/zed/zed_node/rgb/image_rect_color", Image, self.callback)
 
     def callback(self, img_msg):
@@ -16,8 +17,15 @@ class SignDetector:
         np_img = np.frombuffer(img_msg.data, dtype=np.uint8).reshape(img_msg.height, img_msg.width, -1)
         bgr_img = np_img[:,:,:-1]
         rgb_img = cv2.cvtColor(bgr_img, cv2.COLOR_BGR2RGB)
+        ssd = StopSignDetector()
 
-        #TODO: 
+        is_stop,rectangle_coord = ssd.predict(rgb_img)
+        stop_msg = Float32MultiArray()
+        stop_msg.label = "Stop_data"
+        stop_msg.size = 5
+        stop_msg.stride = 1
+        stop_msg.data = [is_stop,rectangle_coord[0],rectangle_coord[1],rectangle_coord[2],rectangle_coord[3]]
+        self.publisher.publish(stop_msg)
 
 if __name__=="__main__":
     rospy.init_node("stop_sign_detector")
